6ad87c67d802dc5ce98414fe52bf71a0a6b422ac
LLMSTRE <161308719+LLMSTRE@users.noreply.github.com>
2024-03-07 09:11:43 +0100

    test.sql

--- test.sql
+++ test.sql
@@ -1,12 +1,12 @@
--- Création de la table 'test'
+-- CrÃ©ation de la table 'test'
 CREATE TABLE test (
     id INT PRIMARY KEY,
     nom VARCHAR(50),
     age INT
 );
 
--- Insertion de données dans la table 'test'
-INSERT INTO test (id, nom, age) VALUES (1, 'Jean', 30);
+-- Insertion de donnÃ©es dans la table 'test'
+INSERT INTO test (id, nom, age) VALUES (1, 'Jean', 29);
 INSERT INTO test (id, nom, age) VALUES (2, 'Marie', 25);
 INSERT INTO test (id, nom, age) VALUES (3, 'Paul', 35);
 INSERT INTO test (id, nom, age) VALUES (4, 'Sophie', 28);
