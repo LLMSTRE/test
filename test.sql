b1769dd2aeca8a1fd14684438eb8fd3d5128d798
LLMSTRE <161308719+LLMSTRE@users.noreply.github.com>
2024-03-07 13:40:34 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,14 +1,1 @@
--- Création de la table 'test'
-CREATE TABLE test (
-    id INT PRIMARY KEY,
-    nom VARCHAR(50),
-    age INT
-);
 
--- Insertion de données dans la table 'test'
-INSERT INTO test (id, nom, age) VALUES (1, 'Jean', 38);
-INSERT INTO test (id, nom, age) VALUES (2, 'Marie', 25);
-INSERT INTO test (id, nom, age) VALUES (3, 'Paul', 35);
-INSERT INTO test (id, nom, age) VALUES (4, 'Sophie', 28);
-
-SELECT * FROM test;
